f677c4a5358dcc8957c2da99c5baff1caadddbdd
QuerkyDoodleCreator <ferreirakonstantin@gmail.com>
2024-11-13 15:03:17 -0800

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🪖 British Army", "🍵 Protect the Tea"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["🪖 British Army"];
+const statusTypes = [ 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
